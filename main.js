7a183252e4ae14ec4a6b358073224cd20ed87ff1
BrunoHRF <BrunoHRF@outlook.com>
2019-09-27 19:53:26 -0300

    Link trocado por button mas ainda com dificuldades para edição

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+var selectedRow = null;
 function onForm(){
     let formData = readFormData();
-    insertData(formData);
+    if(selectedRow == null){
+        insertData(formData);
+    }
     resetData();
 }
 function readFormData(){
@@ -12,7 +15,7 @@ function readFormData(){
     return formData;
 }
 function insertData(data){
-    let table = document.getElementById("userList").getElementsByTagName('tbody')[0];
+    let table = document.getElementById("userList").getElementsByTagName("tbody")[0];
     let newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.userName;
@@ -22,14 +25,20 @@ function insertData(data){
     cell3.innerHTML = data.userSalary;
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.userCity;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML =`<a>Edit</a>
-                    <a>Delete</a>`;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML =`<button onClick ="onEdit(this)">Edit</button>
+                    <button>Delete</button>`;
 }
 function resetData (){
     document.getElementById("userName").value = "";
     document.getElementById("userEmail").value = "";
     document.getElementById("userSalary").value = "";
     document.getElementById("userCity").value = "";
-
+}
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("userName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("userEmail").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("userSalary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("userCity").value = selectedRow.cells[3].innerHTML;
 }
